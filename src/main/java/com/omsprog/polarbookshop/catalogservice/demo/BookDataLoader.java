60e2f187e12ea2b3587f41223660bd45be137be9
omsprog <omsprog@gmail.com>
2024-11-07 00:31:37 -0600

    Setting up PostgreSQL and Spring Data JDBC

--- src/main/java/com/omsprog/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/omsprog/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Profile("testdata") // It will be registered only when the testdata profile is active.
 public class BookDataLoader {
@@ -20,12 +22,12 @@ public class BookDataLoader {
     // The test data generation is triggered when an ApplicationReadyEvent is sent
     @EventListener(ApplicationReadyEvent.class)
     public void loadBookTestData() {
-        var book1 = new Book("1234567891", "Northern Lights",
+        bookRepository.deleteAll();
+        var book1 = Book.of("1234567891", "Northern Lights",
                 "Lyra Silverstar", 9.90);
-        var book2 = new Book("1234567892", "Polar Journey",
+        var book2 = Book.of("1234567892", "Polar Journey",
                 "Iorek Polarson", 12.90);
 
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+        bookRepository.saveAll(List.of(book1, book2));
     }
 }
